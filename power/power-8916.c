CyanogenMod/android_device_qcom_common

power-8916: Enable the perf options for performance mode
Change-Id: If13561ba44668747386dc45d89fe2f2753617b93

power/power-8916.c
@@ -69,7 +69,7 @@ static void set_power_profile(int profile) {
     }
 
     if (profile == PROFILE_HIGH_PERFORMANCE) {
-        int resource_values[] = { SCHED_BOOST_ON, 0x1C00,
+        int resource_values[] = { SCHED_BOOST_ON, 0x1C00, 0x09FF,
             CPU0_MIN_FREQ_TURBO_MAX, CPU1_MIN_FREQ_TURBO_MAX,
             CPU2_MIN_FREQ_TURBO_MAX, CPU3_MIN_FREQ_TURBO_MAX,
             CPU4_MIN_FREQ_TURBO_MAX, CPU5_MIN_FREQ_TURBO_MAX,

CandyDevices/device_qcom_common

power/power-8916.c
@@ -97,7 +97,7 @@ static void set_power_profile(int profile) {
     }
 
     if (profile == PROFILE_HIGH_PERFORMANCE) {
-        int resource_values[] = { SCHED_BOOST_ON, 0x1C00,
+        int resource_values[] = { SCHED_BOOST_ON, 0x1C00, 0x0901,
             CPU0_MIN_FREQ_TURBO_MAX, CPU1_MIN_FREQ_TURBO_MAX,
             CPU2_MIN_FREQ_TURBO_MAX, CPU3_MIN_FREQ_TURBO_MAX,
             CPU4_MIN_FREQ_TURBO_MAX, CPU5_MIN_FREQ_TURBO_MAX,